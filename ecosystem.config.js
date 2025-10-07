25a4fa144e711bd874826d983e6d38cf91105847
Ankit Kumar <ankitpandey841226@gmail.com>
2025-10-07 23:01:03 +0530

    changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,6 +8,13 @@ module.exports = {
       env: {
         NODE_ENV: "production",
         PORT: 5000,
+        MONGO_URI: "mongodb+srv://Ankit:Ankit@cluster0.m609d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+        JWT_SECRET: "supersecretkey_change_me",
+        EMAIL_HOST: "smtp.hostinger.com",
+        EMAIL_PORT: 465,
+        EMAIL_USER: "no-reply@suhtech.in",
+        EMAIL_PASS: "we5#Oy^6:Z5v",
+        FRONTEND_URL: "https://suhtech.in/"
       }
     }
   ]
