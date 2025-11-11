b391940c6c7e3cd6c3cb8c7f32945e24e61d84b7
Ankit8412226 <125825310+Ankit8412226@users.noreply.github.com>
2025-11-11 17:43:42 +0530

    fix

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,14 +7,20 @@ module.exports = {
       exec_mode: "cluster",
       env: {
         NODE_ENV: "production",
-        PORT: 5000,
         MONGO_URI: "mongodb+srv://Ankit:Ankit@cluster0.m609d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
         JWT_SECRET: "supersecretkey_change_me",
+        PORT: "5000",
         EMAIL_HOST: "smtp.hostinger.com",
-        EMAIL_PORT: 465,
+        EMAIL_PORT: "465",
         EMAIL_USER: "no-reply@suhtech.in",
-        EMAIL_PASS: "we5#Oy^6:Z5v",
-        FRONTEND_URL: "https://suhtech.in/"
+        EMAIL_PASS: "Ankit%45@123",
+        FRONTEND_URL: "http://suhtech.in/",
+        BACKEND_URL: "http://localhost:5000",
+
+        // Cashfree Payment Gateway
+        CASHFREE_APP_ID: "your_cashfree_app_id",
+        CASHFREE_SECRET_KEY: "your_cashfree_secret_key",
+        CASHFREE_BASE_URL: "https://sandbox.cashfree.com/pg"
       }
     }
   ]
