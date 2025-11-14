88bc0584d6ef6360592a9fdca9852d680ebe3659
Ankit Kumar <ankitpandey841226@gmail.com>
2025-11-14 14:54:12 +0530

    fixing

--- src/controllers/enrollmentController.js
+++ src/controllers/enrollmentController.js
@@ -13,3 +13,60 @@ export const myEnrollments = catchAsync(async (req, res) => {
   const enrollments = await Enrollment.find({ student: req.user._id }).populate("course");
   res.json({ success: true, data: enrollments });
 });
+
+export const getEnrollmentStats = async (req, res) => {
+  try {
+    const stats = await Enrollment.aggregate([
+      {
+        $lookup: {
+          from: "courses",
+          localField: "course",
+          foreignField: "_id",
+          as: "courseData"
+        }
+      },
+      { $unwind: "$courseData" },
+      {
+        $group: {
+          _id: "$course",
+          courseName: { $first: "$courseData.title" },
+          totalStudents: { $sum: 1 },
+          avgProgress: { $avg: "$progress" },
+          enrolled: {
+            $sum: { $cond: [{ $eq: ["$status", "enrolled"] }, 1, 0] }
+          },
+          completed: {
+            $sum: { $cond: [{ $eq: ["$status", "completed"] }, 1, 0] }
+          },
+          cancelled: {
+            $sum: { $cond: [{ $eq: ["$status", "cancelled"] }, 1, 0] }
+          }
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          courseId: "$_id",
+          courseName: 1,
+          totalStudents: 1,
+          avgProgress: { $round: ["$avgProgress", 2] },
+          enrolled: 1,
+          completed: 1,
+          cancelled: 1
+        }
+      }
+    ]);
+
+    res.status(200).json({
+      success: true,
+      data: stats
+    });
+  } catch (error) {
+    console.error("Error in getEnrollmentStats:", error);
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+      error: error.message
+    });
+  }
+};
--- src/routes/enrollmentRoutes.js
+++ src/routes/enrollmentRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { enroll, myEnrollments } from "../controllers/enrollmentController.js";
-import { protect } from "../middlewares/authMiddleware.js";
+import { enroll, getEnrollmentStats, myEnrollments } from "../controllers/enrollmentController.js";
+import { authorizeRoles, protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", protect, enroll);
 router.get("/me", protect, myEnrollments);
+router.get("/enrolment-number-course-wise" , protect ,   authorizeRoles("admin"),getEnrollmentStats  )
 
 export default router;
